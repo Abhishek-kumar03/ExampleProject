736f0466c18d9d0710de3f16bbd38f43bfd0f41f
Abhishek-kumar03 <abhishekmaravanthe527527@gmail.com>
2025-02-10 15:20:41 +0530

    First file Added

---
+++ DataTypeExample.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class DataTypeExample{
+	public static void main(String args[]){
+		float mark=34.34f;
+		char grade='A';
+		short age=21;
+		boolean status=true;
+		System.out.println(mark);
+		System.out.println(grade);
+		System.out.println(age);
+		System.out.println(status);
+}
+}
